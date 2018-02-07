da7eb659c549e30eb6caeb517282478c1745aa4f
pz-zhao <pz-zhao@outlook.com>
2018-02-07 14:22:50 +0800

    tiaozhan3

--- calculator.py
+++ calculator.py
@@ -15,14 +15,14 @@ class Args:
          try:
             #get value of parameters by index 
              value=self.args[self.args.index(arg)+1]
-             except (ValueError,IndexError):
-                 value=None
-  	     return value
+         except (ValueError,IndexError):
+             value=None
+         return value
     def get_arg(self,arg):
-          value=self.__parse_arg(arg)
-	  if value is None:
-	      raise ArgError('not found ars s%',%arg)
-	  return value 
+        value=self.__parse_arg(arg)
+        if value is None:
+            raise ArgError('not found ars s%' % arg)
+        return value 
 
 class SheBaoConfig:
 ##shebao config file class
@@ -35,35 +35,134 @@ class SheBaoConfig:
         jishu_high=0
 
         with open(file) as f:
-	    for line in f:
- 	        key,value=line.split('=')
-  		key=key.strip()
-		try:
-		    value=float(value.strip())
-		except ValueError:
-		    continue
-		if key=='JiShuL':
-		    jishu_low=value
-		elif key=='JiShuH':
-		    jishu_high=value
-		else:
-		    rate+=value
-	return jishu_low,jishu_high,rate
+            for line in f:
+                key,value=line.split('=')
+                key=key.strip()
+                try:
+                    value=float(value.strip())
+                except ValueError:
+                    continue
+                if key=='JiShuL':
+                    jishu_low=value
+                elif key=='JiShuH':
+                    jishu_high=value
+                else:
+                    rate+=value
+        return jishu_low,jishu_high,rate
 
 class EmployeeData:
 ##employee data achieve class
     def __init__(self,file):
 	
-	self.data=self.__parse_file(file)
+        self.data=self.__parse_file(file)
     def __parse_file(self,file):
-	data=[]
-	for line in open(file):
+        data=[]
+        for line in open(file):
             employee_id,gongzi=line.split(',')
             data.append((int(employee_id),int(gongzi)))
-	return data
+        return data
 
     def __iter__(self):
+        return iter(self.data)   
+
+class Calculator:
+
+    tax_start=3500
+    tax_table=[
+        (80000,0.45,13505),
+        (55000,0.35,5505),
+        (35000,0.3,5505),
+        (9000,0.25,1005),
+        (4500,0.2,555),
+        (1500,0.1,105),
+        (0,0.03,0),
+]
+
+    def __init__(self,config):
+        self.config=config
+    def calculate(self,data_item):
+        employee_id,gongzi=data_item
         
+        if gongzi<self.config.jishu_low:
+            shebao=self.config.jishu_low*self.config.total_rate
+        elif gongzi>self.config.jishu_high:
+            shebao=self.config.jishu_high*self.config.total_rate
+        else:
+            shebao=gongzi*self.config.total_rate 
+
+        left_gongzi=gongzi-shebao
+        tax_gongzi=left_gongzi-self.tax_start 
+
+        if tax_gongzi<0:
+             tax=0
+        else: 
+             for item in self.tax_table:
+                 if tax_gongzi>item[0]:
+                     tax_gongzi*item[1]-item[2]  
+                     break
+
+# last gongzi=gongzi-shebao-self_tax 
+        last_gongzi=left_gongzi-tax
+        return str(employee_id),str(gongzi),'{:.2f}'.format(shebao),'{:.2f}'.format(tax),'{:.2f}',format(last_gongzi)
+
+
+class Exporter:
+## dao chu lei shi xian 
+    def __init__(self,file):
+        self.file=file
+    def export(self,data):
+        for item in data:
+            line=','.join(item)+'\n'
+            content+=line
+        with open(self.file,'w') as f:
+            f.write(content)
+
+
+class Executor:
+##zhixing qi 
+    def __init__(self,args):
+        args=Args(args)
+        config=SheBaoConfig(args.get_arg('-c'))
+        self.employee_data=EmployeeData(args.get_arg('-d'))
+        self.exporter=Exporter(args.get_arg('-o'))
+        self.calculator=Calculator(config)
+
+    def execute(self):
+        results=[] 
+        for item in self.employee_data:
+            result=self.calculator.calculate(item)
+            results.append(result)
+        self.exporter.export(results)
+
+if __name__=='__main__':
+    executor = Executor(sys.argv[1:])
+    executor.execute() 
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
